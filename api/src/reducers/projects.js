fc3ef7a673a61577fc0fe8fc594885816704b088
devholic <devholic@plusquare.com>
2016-04-25 16:53:14 +0900

    front: add project list reducer

---
+++ api/src/reducers/projects.js
@@ -0,0 +1,38 @@
+import {
+  PROJECTS_FETCH_ERROR,
+  PROJECTS_FETCHING,
+  PROJECTS_FETCHED,
+} from 'constants/projects';
+
+const initialState = {
+  fetching: false,
+  list: [],
+};
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+
+    case PROJECTS_FETCH_ERROR:
+      return {
+        ...state,
+        fetching: false,
+        list: [],
+      };
+
+    case PROJECTS_FETCHING:
+      return {
+        ...state,
+        fetching: true,
+      };
+
+    case PROJECTS_FETCHED:
+      return {
+        ...state,
+        fetching: false,
+        list: action.projects,
+      };
+
+    default:
+      return state;
+  }
+}
