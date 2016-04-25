489ff0237876c0af1606351fd6b529955a1dc8d4
devholic <devholic@plusquare.com>
2016-04-25 17:58:27 +0900

    front: add client entry point

---
+++ api/src/client.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import routes from 'routes';
+import configureStore from 'util/redux/createStore';
+
+const appContainer = document.getElementById('app');
+const store = configureStore(browserHistory);
+const history = syncHistoryWithStore(browserHistory, store);
+
+function run() {
+  render(
+    (
+      <Provider store={store}>
+        <Router history={history} routes={routes(store)} />
+      </Provider>
+    ),
+    appContainer
+  );
+}
+
+if (['complete', 'loaded', 'interactive'].includes(document.readyState) && document.body) {
+  run();
+} else {
+  document.addEventListener('DOMContentLoaded', run, false);
+}
