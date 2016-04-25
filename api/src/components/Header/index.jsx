ba9cdfc2005ecb159c70a6849dea71da73f826d5
devholic <devholic@plusquare.com>
2016-04-25 17:21:37 +0900

    front: add Header component

---
+++ api/src/components/Header/index.jsx
@@ -0,0 +1,98 @@
+// React
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Actions
+import Actions from 'actions/Auth';
+
+class Header extends React.Component {
+  _logout() {
+    const { dispatch } = this.props;
+    dispatch(Actions.logout());
+  }
+
+  _renderHeader() {
+    switch (this.props.page) {
+      case 'login':
+        return (
+          <div>
+            <ul className="header-left">
+              <li>
+                <a href="/">GONI</a>
+              </li>
+            </ul>
+            <ul className="header-right">
+              <li>
+                <a href="https://github.com/layer123/goni"><i className="fa fa-github" aria-hidden="true" /> GITHUB</a>
+              </li>
+              <li>
+                <a href="/register">REGISTER</a>
+              </li>
+            </ul>
+          </div>
+        );
+      case 'register':
+        return (
+          <div>
+            <ul className="header-left">
+              <li>
+                <a href="/">GONI</a>
+              </li>
+            </ul>
+            <ul className="header-right">
+              <li>
+                <a href="https://github.com/layer123/goni"><i className="fa fa-github" aria-hidden="true" /> GITHUB</a>
+              </li>
+              <li>
+                <a href="/login">LOGIN</a>
+              </li>
+            </ul>
+          </div>
+        );
+      default:
+        return (
+          <div>
+            <ul className="header-left">
+              <li>
+                <a href="/">GONI</a>
+              </li>
+            </ul>
+            <ul className="header-right">
+              <li>
+                <a href="https://github.com/layer123/goni"><i className="fa fa-github" aria-hidden="true" /> GITHUB</a>
+              </li>
+              <li>
+                <a href="#">MYPAGE</a>
+              </li>
+              <li>
+                <a href="#" onClick={() => this._logout()}>LOGOUT</a>
+              </li>
+            </ul>
+          </div>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <div className="header">
+        {this._renderHeader()}
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  currentUser: React.PropTypes.object,
+  dispatch: React.PropTypes.func.isRequired,
+  isDashboard: React.PropTypes.bool.isRequired,
+  page: React.PropTypes.string,
+};
+
+Header.defaultProps = {
+  isDashboard: false,
+};
+
+const mapStateToProps = (state) => ({ currentUser: state.auth.currentUser });
+
+export default connect(mapStateToProps)(Header);
