3a08298ad9ee6784d05f136e2a85905c5014b489
devholic <devholic@plusquare.com>
2016-05-02 14:54:27 +0900

    front: update project reducer add dashboard support

--- api/src/reducers/project.js
+++ api/src/reducers/project.js
@@ -1,12 +1,17 @@
 import {
+  PROJECT_CHANGE_DASHBOARD,
   PROJECT_CLEAR,
   PROJECT_ENTER,
+  PROJECT_ENTER_GONI,
+  PROJECT_ENTER_GONIPLUS,
   PROJECT_FETCH_ERROR,
   PROJECT_FETCHED,
   PROJECT_FETCHING,
 } from 'constants/project';
 
 const initialState = {
+  currentDashboard: '',
+  currentDuration: '6h',
   currentProject: null,
   fetching: false,
 };
@@ -16,10 +21,28 @@ export default function reducer(state = initialState, action = {}) {
     case PROJECT_CLEAR:
       return initialState;
 
+    case PROJECT_CHANGE_DASHBOARD:
+      return {
+        ...state,
+        currentDashboard: action.selected,
+      };
+
     case PROJECT_ENTER:
       return {
         ...state,
         currentProject: action.project,
+      };
+
+    case PROJECT_ENTER_GONI:
+      return {
+        ...state,
+        currentDashboard: 'Instances',
+      };
+
+    case PROJECT_ENTER_GONIPLUS:
+      return {
+        ...state,
+        currentDashboard: 'Runtime',
       };
 
     case PROJECT_FETCH_ERROR:
