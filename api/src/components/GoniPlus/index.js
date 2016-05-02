9685696266a7f06f45243e0ddaa5fb164e976c91
devholic <devholic@plusquare.com>
2016-05-02 17:32:04 +0900

    front: Update goniplus dashboard

--- api/src/components/GoniPlus/index.js
+++ api/src/components/GoniPlus/index.js
@@ -2,21 +2,87 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// Actions
-// import Actions from 'actions/Project';
-
 // Components
 import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
+import { Expvar, Runtime } from 'components/Metric';
+
+import {
+  PROJECT_ENTER_GONIPLUS,
+} from 'constants/project';
+
+const sidebar = [
+  {
+    header: 'metrics',
+    item: [
+      'Expvar',
+      'Runtime',
+    ],
+  },
+  {
+    header: 'API',
+    item: [
+      'Response',
+      'Statistics',
+    ],
+  },
+  {
+    header: 'SETTINGS',
+    item: [
+      'Guide',
+    ],
+  },
+];
 
 class GoniPlus extends React.Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: PROJECT_ENTER_GONIPLUS,
+    });
+  }
+
+  _renderGraphs() {
+    const { currentDashboard } = this.props;
+    switch (currentDashboard) {
+      case 'Expvar':
+        return <Expvar />;
+      case 'Runtime':
+        return <Runtime />;
+      default:
+        return <Runtime />;
+    }
+  }
+
   _renderLayout() {
     const { currentProject, fetching } = this.props;
     return (
       <div className="child">
-        <div className="dashboard">
+        <Sidebar menu={sidebar} />
+        <div className="dashboard-sidebar">
           <div className="dashboard-header">
-            <h1>{ currentProject.name } { fetching ? <i className="fa fa-circle-o-notch fa-spin" aria-hidden="true" /> : null }</h1>
+            <h1>{ currentProject.name }
+              { fetching ?
+                <i className="fa fa-circle-o-notch fa-spin" aria-hidden="true" /> :
+                null
+              }
+            </h1>
+            <div className="tag-tab-wrap">
+              <div className="tag-tab">
+                <a>30 MINUTES</a>
+              </div>
+              <div className="tag-tab-more">
+                <a>1 HOUR</a>
+              </div>
+              <div className="tag-tab-more">
+                <a>3 HOURS</a>
+              </div>
+              <div className="tag-tab-more">
+                <a>6 HOURS</a>
+              </div>
+            </div>
           </div>
+          {this._renderGraphs()}
         </div>
       </div>
     );
@@ -37,12 +103,14 @@ class GoniPlus extends React.Component {
 }
 
 GoniPlus.propTypes = {
+  currentDashboard: React.PropTypes.string,
   currentProject: React.PropTypes.object,
   dispatch: React.PropTypes.func.isRequired,
   fetching: React.PropTypes.bool,
 };
 
 const mapStateToProps = (state) => ({
+  currentDashboard: state.project.currentDashboard,
   currentProject: state.project.currentProject,
   fetching: state.project.fetching,
 });
