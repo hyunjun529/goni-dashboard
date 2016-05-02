9550fe932b3baef2d1e66975eab83f6e64be63d1
devholic <devholic@plusquare.com>
2016-05-02 14:53:22 +0900

    api: add metric constants

---
+++ api/src/constants/metric/index.js
@@ -0,0 +1,5 @@
+// Metric
+export const METRIC_CLEAR = 'METRIC_CLEAR';
+export const METRIC_FETCHED = 'METRIC_FETCHED';
+export const METRIC_FETCHING = 'METRIC_FETCHING';
+export const METRIC_FETCH_ERROR = 'METRIC_FETCH_ERROR';
