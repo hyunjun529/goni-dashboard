2f6563f5a2ac8b666b41cdfaf6c142da75c7e9d4
devholic <devholic@plusquare.com>
2016-05-04 18:16:11 +0900

    front: add instance select support to metric reducer

--- api/src/reducers/metric.js
+++ api/src/reducers/metric.js
@@ -1,23 +1,46 @@
 import {
+  METRIC_CHANGE_INSTANCE,
   METRIC_CLEAR,
+  METRIC_CLEAR_ALL,
   METRIC_FETCH_ERROR,
   METRIC_FETCHED,
   METRIC_FETCHING,
+  METRIC_INSTANCE_FETCHED,
+  METRIC_INSTANCE_FETCHING,
+  METRIC_INSTANCE_FETCH_ERROR,
 } from 'constants/metric';
 
 const initialState = {
+  currentInstance: null,
   errored: false,
   fetchedData: null,
+  fetchedInstances: null,
   fetching: false,
+  instanceFetching: false,
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
+    case METRIC_CHANGE_INSTANCE:
+      return {
+        ...state,
+        currentInstance: action.instance,
+      };
+
     case METRIC_CLEAR:
       return {
         ...state,
         errored: false,
         fetchedData: null,
+      };
+
+    case METRIC_CLEAR_ALL:
+      return {
+        ...state,
+        currentInstance: null,
+        errored: false,
+        fetchedData: null,
+        fetchedInstances: null,
       };
 
     case METRIC_FETCH_ERROR:
@@ -41,6 +64,29 @@ export default function reducer(state = initialState, action = {}) {
         errored: false,
         fetchedData: null,
         fetching: true,
+      };
+
+    case METRIC_INSTANCE_FETCH_ERROR:
+      return {
+        ...state,
+        errored: true,
+        fetchedInstances: null,
+        instanceFetching: false,
+      };
+
+    case METRIC_INSTANCE_FETCHED:
+      return {
+        ...state,
+        fetchedInstances: action.instances,
+        instanceFetching: false,
+      };
+
+    case METRIC_INSTANCE_FETCHING:
+      return {
+        ...state,
+        errored: false,
+        fetchedInstances: null,
+        instanceFetching: true,
       };
 
     default:
