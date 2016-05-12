c524d681ee36bbbe84af864f18292519a0726d70
devholic <devholic@plusquare.com>
2016-05-12 18:37:54 +0900

    front: add constant for clear error in auth page

--- api/src/constants/auth/index.js
+++ api/src/constants/auth/index.js
@@ -1,4 +1,5 @@
 // Auth
+export const AUTH_CLEAR_ERROR = 'AUTH_CLEAR_ERROR';
 export const AUTH_CURRENT_USER = 'AUTH_CURRENT_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const AUTH_LOGIN = 'AUTH_LOGIN';
