e0b41d3486ca2924abeb03da273cdf97957bab2a
devholic <devholic@plusquare.com>
2016-05-14 17:41:40 +0900

    api: add canAccessProject for check permission / add getProjectMemberList for member inviting support

--- api/src/backend/api/project/core.js
+++ api/src/backend/api/project/core.js
@@ -14,6 +14,70 @@ const projectRoleColumn = '(project_id,user_id,user_role)';
 const projectRoleField = '(?,?,?)';
 
 /**
+ * canAccessProject(id, user) returns true
+ * if user can access project
+ *
+ * @param {Integer} project id
+ * @param {Integer} user id
+ * @return {Boolean} canAccess
+ */
+export function canAccessProject(id, user) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((connErr, connection) => {
+      if (connErr) {
+        reject(connErr);
+      }
+      connection.query({
+        sql: 'SELECT COUNT(DISTINCT project_id) FROM project_role WHERE project_id=? AND user_id=?',
+        values: [id, user],
+      }, (err, results) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        }
+        if (results.length === 0) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  });
+}
+
+/**
+ * canAccessProject(id, user) returns true
+ * if user can access project
+ *
+ * @param {String} project apikey
+ * @param {Integer} user id
+ * @return {Boolean} canAccess
+ */
+export function canAccessProjectByKey(key, user) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((connErr, connection) => {
+      if (connErr) {
+        reject(connErr);
+      }
+      connection.query({
+        sql: 'SELECT COUNT(DISTINCT project.id) FROM project JOIN project_role ON project.id = project_role.project_id WHERE project.apikey=? AND project_role.user_id=?;',
+        values: [key, user],
+      }, (err, results) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        }
+        if (results.length === 0) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  });
+}
+
+/**
  * createProject(name, isPlus, user) returns project id
  *
  * @param {String} project name
@@ -113,6 +177,32 @@ export function getProjectList(id) {
       }
       connection.query({
         sql: 'SELECT DISTINCT project.id, project.name, project.is_plus, project.apikey FROM project JOIN project_role ON project.id = project_role.project_id WHERE project_role.user_id = ?',
+        values: [id],
+      }, (err, results) => {
+        connection.release();
+        if (err) {
+          reject(err);
+        }
+        resolve(results);
+      });
+    });
+  });
+}
+
+/**
+ * getProjectMemberList(id) returns project's member list
+ *
+ * @param {Integer} project id
+ * @return {Array} {Object} user id(Integer), user email(String), user role(Integer)
+ */
+export function getProjectMemberList(id) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((connErr, connection) => {
+      if (connErr) {
+        reject(connErr);
+      }
+      connection.query({
+        sql: 'SELECT DISTINCT user.id, user.email, project_role.user_role FROM user JOIN project_role ON user.id = project_role.user_id WHERE project_role.project_id = ?',
         values: [id],
       }, (err, results) => {
         connection.release();
