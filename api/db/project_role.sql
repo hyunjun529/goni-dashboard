06ba8dc477dfd118408a8792c0686bcfb2173b7b
devholic <devholic@plusquare.com>
2016-05-14 17:49:55 +0900

    db: remove primary key from project_role

--- api/db/project_role.sql
+++ api/db/project_role.sql
@@ -1,6 +1,5 @@
 CREATE TABLE `project_role` (
   `project_id` int(11) unsigned NOT NULL,
   `user_id` int(11) unsigned NOT NULL,
-  `user_role` tinyint(11) unsigned NOT NULL,
-  PRIMARY KEY (`project_id`)
+  `user_role` tinyint(11) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
