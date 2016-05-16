c599466d62417ca6fdc5144d0b0b1a6410d4b7ac
devholic <devholic@plusquare.com>
2016-05-16 14:41:46 +0900

    front: add action, reducer for handling setting page / add, remove member support / add new menu

--- api/src/frontend/actions/Project/index.js
+++ api/src/frontend/actions/Project/index.js
@@ -1,12 +1,26 @@
+import _ from 'lodash';
 import {
   PROJECT_FETCHED,
   PROJECT_FETCHING,
+  PROJECT_MEMBER_FETCHED,
+  PROJECT_MEMBER_FETCHING,
+  PROJECT_MEMBER_FETCH_ERROR,
+  PROJECT_MEMBER_UPDATED,
+  PROJECT_MODAL_CLOSE,
+  PROJECT_MODAL_OPEN,
+  PROJECT_MODAL_TYPE,
+  PROJECT_SETTING_REQUEST,
+  PROJECT_SETTING_REQUEST_CLEAR_ERROR,
+  PROJECT_SETTING_REQUEST_END,
+  PROJECT_SETTING_REQUEST_ERROR,
 } from 'constants/project';
 import {
   push,
 } from 'react-router-redux';
 import {
+  httpDelete,
   httpGet,
+  httpPost,
 } from 'frontend/util/fetch';
 
 const PROJECT_URL = '/api/project';
@@ -16,6 +30,33 @@ function getProjectUrl(id) {
 }
 
 const Actions = {
+  addUserToProject: (id, data, member) => {
+    return async dispatch => {
+      try {
+        dispatch({
+          type: PROJECT_SETTING_REQUEST,
+        });
+        const token = localStorage.getItem('token');
+        const res = await httpPost(`${getProjectUrl(id)}/member`, token, data);
+        dispatch({
+          type: PROJECT_SETTING_REQUEST_END,
+        });
+        member.push(res);
+        dispatch({
+          type: PROJECT_MEMBER_UPDATED,
+          data: member,
+        });
+        dispatch({
+          type: PROJECT_MODAL_CLOSE,
+        });
+      } catch (error) {
+        dispatch({
+          type: PROJECT_SETTING_REQUEST_ERROR,
+          error: error.statusText,
+        });
+      }
+    };
+  },
   getProject: (id) => {
     return async dispatch => {
       try {
@@ -30,6 +71,70 @@ const Actions = {
         });
       } catch (error) {
         dispatch(push('/'));
+      }
+    };
+  },
+  getProjectMemberList: (id) => {
+    return async dispatch => {
+      try {
+        dispatch({
+          type: PROJECT_MEMBER_FETCHING,
+        });
+        const token = localStorage.getItem('token');
+        const res = await httpGet(`${getProjectUrl(id)}/member`, token);
+        dispatch({
+          type: PROJECT_MEMBER_FETCHED,
+          member: res,
+        });
+      } catch (error) {
+        dispatch({
+          type: PROJECT_MEMBER_FETCH_ERROR,
+        });
+      }
+    };
+  },
+  openModal: (modal, data) => {
+    return async dispatch => {
+      dispatch({
+        type: PROJECT_MODAL_TYPE,
+        data,
+        modal,
+      });
+      dispatch({
+        type: PROJECT_SETTING_REQUEST_CLEAR_ERROR,
+      });
+      dispatch({
+        type: PROJECT_MODAL_OPEN,
+      });
+    };
+  },
+  removeUserFromProject: (id, user, member) => {
+    return async dispatch => {
+      try {
+        dispatch({
+          type: PROJECT_SETTING_REQUEST,
+        });
+        const token = localStorage.getItem('token');
+        await httpDelete(`${getProjectUrl(id)}/member/${user}`, token);
+        const memberIdx = _.findIndex(member, (o) => {
+          return o.id === user;
+        });
+        dispatch({
+          type: PROJECT_SETTING_REQUEST_END,
+        });
+        member.splice(memberIdx, 1);
+        dispatch({
+          type: PROJECT_MEMBER_UPDATED,
+          data: member,
+        });
+        dispatch({
+          type: PROJECT_MODAL_CLOSE,
+        });
+      } catch (error) {
+        dispatch({
+          type: PROJECT_SETTING_REQUEST_ERROR,
+          error: error.statusText,
+        });
       }
     };
   },
--- api/src/frontend/reducers/project.js
+++ api/src/frontend/reducers/project.js
@@ -5,16 +5,37 @@ import {
   PROJECT_ENTER,
   PROJECT_ENTER_GONI,
   PROJECT_ENTER_GONIPLUS,
+  PROJECT_ENTER_METRIC_PAGE,
+  PROJECT_ENTER_NON_METRIC_PAGE,
   PROJECT_FETCH_ERROR,
   PROJECT_FETCHED,
   PROJECT_FETCHING,
+  PROJECT_MEMBER_FETCH_ERROR,
+  PROJECT_MEMBER_FETCHED,
+  PROJECT_MEMBER_FETCHING,
+  PROJECT_MEMBER_UPDATED,
+  PROJECT_MODAL_OPEN,
+  PROJECT_MODAL_CLOSE,
+  PROJECT_MODAL_TYPE,
+  PROJECT_SETTING_REQUEST,
+  PROJECT_SETTING_REQUEST_CLEAR_ERROR,
+  PROJECT_SETTING_REQUEST_END,
+  PROJECT_SETTING_REQUEST_ERROR,
 } from 'constants/project';
 
 const initialState = {
   currentDashboard: '',
   currentDuration: '6h',
   currentProject: null,
+  isMetricPage: false,
+  isModalOpen: false,
   fetching: false,
+  member: null,
+  memberFetching: false,
+  modalData: null,
+  modalType: '',
+  settingError: null,
+  settingRequested: false,
 };
 
 export default function reducer(state = initialState, action = {}) {
@@ -52,6 +73,18 @@ export default function reducer(state = initialState, action = {}) {
         currentDashboard: 'Runtime',
       };
 
+    case PROJECT_ENTER_METRIC_PAGE:
+      return {
+        ...state,
+        isMetricPage: true,
+      };
+
+    case PROJECT_ENTER_NON_METRIC_PAGE:
+      return {
+        ...state,
+        isMetricPage: false,
+      };
+
     case PROJECT_FETCH_ERROR:
       return {
         ...state,
@@ -71,6 +104,74 @@ export default function reducer(state = initialState, action = {}) {
         ...state,
         currentProject: null,
         fetching: true,
+      };
+
+    case PROJECT_MEMBER_FETCH_ERROR:
+      return {
+        ...state,
+        member: null,
+        memberFetching: false,
+      };
+
+    case PROJECT_MEMBER_FETCHED:
+      return {
+        ...state,
+        member: action.member,
+        memberFetching: false,
+      };
+
+    case PROJECT_MEMBER_FETCHING:
+      return {
+        ...state,
+        member: null,
+        memberFetching: true,
+      };
+
+    case PROJECT_MEMBER_UPDATED:
+      return {
+        ...state,
+        member: action.data,
+      };
+
+    case PROJECT_MODAL_OPEN:
+      return {
+        ...state,
+        isModalOpen: true,
+      };
+
+    case PROJECT_MODAL_CLOSE:
+      return {
+        ...state,
+        isModalOpen: false,
+      };
+
+    case PROJECT_MODAL_TYPE:
+      return {
+        ...state,
+        modalType: action.modal,
+        modalData: action.data,
+      };
+
+    case PROJECT_SETTING_REQUEST:
+      return {
+        ...state,
+        settingError: null,
+        settingRequested: true,
+      };
+
+    case PROJECT_SETTING_REQUEST_CLEAR_ERROR:
+    case PROJECT_SETTING_REQUEST_END:
+      return {
+        ...state,
+        settingError: null,
+        settingRequested: false,
+      };
+
+    case PROJECT_SETTING_REQUEST_ERROR:
+      return {
+        ...state,
+        settingError: action.error,
+        settingRequested: false,
       };
 
     default:
