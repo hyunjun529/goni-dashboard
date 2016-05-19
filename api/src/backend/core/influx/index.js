776bc17faaf07f90fb4d32d71e02d51aa868e185
devholic <devholic@plusquare.com>
2016-05-19 22:46:44 +0900

    api: parse influxdb / mysql port value as integer

--- api/src/backend/core/influx/index.js
+++ api/src/backend/core/influx/index.js
@@ -1,7 +1,7 @@
 import influx from 'influx';
 
 const influxHost = process.env.GONI_INFLUX_HOST || 'influxHost';
-const influxPort = process.env.GONI_INFLUX_PORT || 8086;
+const influxPort = process.env.GONI_INFLUX_PORT ? parseInt(process.env.GONI_INFLUX_PORT, 10) : 8086;
 const influxProtocol = 'http';
 const influxUser = process.env.GONI_INFLUX_USER || 'influxUser';
 const influxPass = process.env.GONI_INFLUX_PASS || 'influxPass';
--- api/src/backend/core/mysql/index.js
+++ api/src/backend/core/mysql/index.js
@@ -2,7 +2,7 @@ import mysql from 'mysql';
 
 const pool = mysql.createPool({
   host: process.env.GONI_MYSQL_HOST || 'host',
-  port: process.env.GONI_MYSQL_PORT || 3306,
+  port: process.env.GONI_MYSQL_PORT ? parseInt(process.env.GONI_MYSQL_PORT, 10) : 3306,
   user: process.env.GONI_MYSQL_USER || 'user',
   password: process.env.GONI_MYSQL_PASS || 'password',
   database: 'goni_saas',
