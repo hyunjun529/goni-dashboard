66472103abaf436140ed9e7e99274d07f168b9ca
devholic <devholic@plusquare.com>
2016-06-02 12:13:45 +0900

    worker: fix bug / slow transaction support

--- worker/src/goniplusPushWorker.js
+++ worker/src/goniplusPushWorker.js
@@ -24,8 +24,8 @@ const pool = mysql.createPool({
 });
 
 const COLOR_ALERT = '#ff7595';
-const COLOR_WARNING = '#ffda00'; // eslint-disable-line no-unused-vars
-const COLOR_NORMAL = '#4c80f1';
+const COLOR_WARNING = '#ffda00';
+const COLOR_NORMAL = '#4c80f1'; // eslint-disable-line no-unused-vars
 
 function createMessage(data) {
   let isNormal = true;
@@ -40,7 +40,22 @@ function createMessage(data) {
       author_name: k,
       fields: [{
         title: 'Panic Alert',
-        value: `${v} transactions`,
+        value: `${v} transaction(s)`,
+        short: false,
+      }],
+      footer: data.instance,
+      ts: data.date,
+    });
+  });
+  _.forEach(data.httpSlow, (v, k) => {
+    isNormal = false;
+    body.attachments.push({
+      fallback: `[Slow Transaction Alert] ${k}`,
+      color: COLOR_WARNING,
+      author_name: k,
+      fields: [{
+        title: 'Slow Transaction Alert',
+        value: `${v} transaction(s)`,
         short: false,
       }],
       footer: data.instance,
@@ -48,32 +63,26 @@ function createMessage(data) {
     });
   });
   if (isNormal) {
-    body.attachments.push({
-      fallback: 'Service operational',
-      color: COLOR_NORMAL,
-      fields: [{
-        title: 'Service operational',
-        short: false,
-      }],
-      footer: data.instance,
-      ts: data.date,
-    });
+    return null;
   }
   return body;
 }
 
 function sendSlackNotification(url, data) {
   return new Promise((resolve, reject) => {
-    request({
+    const json = createMessage(data);
+    if (!json) {
+      return resolve(null);
+    }
+    return request({
       url,
       method: 'POST',
       json: createMessage(data),
     }, (err, resp) => {
       if (!err && resp.statusCode === 200) {
-        resolve(null);
-      } else {
-        reject(err);
+        return resolve(null);
       }
+      return reject(err);
     });
   });
 }
@@ -124,25 +133,24 @@ amqp.connect(`amqp://${queueUser}:${queuePass}@${queueHost}:${queuePort}`, (conn
         const data = JSON.parse(msg.content);
         const slackUrl = await getSlackHookUrl(data.apikey);
         const httpPanic = {};
+        const httpSlow = {};
         _.forEach(data.app.http, (metric, path) => {
-          _.forEach(metric, (resp, method) => {
+          _.forEach(metric, (resp, method) => { // eslint-disable-line no-unused-vars
             _.forEach(resp, (result, code) => { // eslint-disable-line no-unused-vars
-              _.forEach(result, (res, browser) => {
+              _.forEach(result, (res, browser) => { // eslint-disable-line no-unused-vars
                 _.forEach(res, (v) => {
-                  const d = {
-                    time: new Date(+v.time * 1000),
-                    browser,
-                    method,
-                    instance: data.instance,
-                    res: v.res,
-                  };
                   if (v.panic) {
-                    d.panic = true;
+                    if (!httpPanic[path]) {
+                      httpPanic[path] = 0;
+                    }
+                    httpPanic[path]++;
                   }
-                  if (!httpPanic[path]) {
-                    httpPanic[path] = 0;
+                  if (v.res >= 3000) {
+                    if (!httpSlow[path]) {
+                      httpSlow[path] = 0;
+                    }
+                    httpSlow[path]++;
                   }
-                  httpPanic[path]++;
                 });
               });
             });
@@ -152,6 +160,7 @@ amqp.connect(`amqp://${queueUser}:${queuePass}@${queueHost}:${queuePort}`, (conn
           date: getTimestamp(data.time),
           instance: data.instance,
           httpPanic,
+          httpSlow,
         });
         ch.ack(msg);
       } catch (error) {
